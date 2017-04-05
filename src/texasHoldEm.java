fdcc301198cf6b0ad40438dab13febc7dfe25df8
chillhumanoid <jonathanthorne13@gmail.com>
2017-04-05 15:58:29 -0400

    displays deals. feel free to download and build. not putting a runnable file out til its playable.

--- src/texasHoldEm.java
+++ src/texasHoldEm.java
@@ -47,24 +47,58 @@ public class texasHoldEm{
     private static String comp8name = null;
     private static String uName = null;
     //private static Users user = new Users(uName);
-    public static void main(String args[]){
-        cardHandler deck = new cardHandler();
-        deck.shuffleDeck();
-        deck.printDeck();
+    public static void main(String args[]) throws InterruptedException, IOException{
+        play();
     }
     public static void play() throws InterruptedException, IOException{
-        new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+        //new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
         deck.shuffleDeck();
         currentPlayer = 1;
         for(int i = 0; i < 2; i++){
             play1.addCard(deck);
             comp1.addCard(deck);
+            comp2.addCard(deck);
             comp3.addCard(deck);
             comp4.addCard(deck);
             comp5.addCard(deck);
             comp6.addCard(deck);
             comp7.addCard(deck);
             comp8.addCard(deck);
+        }
+        System.out.println();
+        System.out.println("Play1");
+        System.out.println();
+        printHand(play1);
+        System.out.println();
+        System.out.println("Comp1");
+        printHand(comp1);
+        System.out.println();
+        System.out.println("Comp2");
+        printHand(comp2);
+        System.out.println();
+        System.out.println("Comp3");
+        printHand(comp3);
+        System.out.println();
+        System.out.println("Comp4");
+        printHand(comp4);
+        System.out.println();
+        System.out.println("Comp5");
+        printHand(comp5);
+        System.out.println();
+        System.out.println("Comp6");
+        printHand(comp6);
+        System.out.println();
+        System.out.println("Comp7");
+        printHand(comp7);
+        System.out.println();
+        System.out.println("Comp8");
+        printHand(comp8);
+    }
+    private static void printHand(deal play1){
+        int display = 0;
+        for(int x = 0; x < play1.getSize(); x++){
+            display = x+1;
+            System.out.println(display + ". " + play1.getCard(x));
         }
     }
 }
